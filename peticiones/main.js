4db9444fb7fa730c1630f725709dd92f71026528
Masavi <masavi2008@hotmail.com>
2018-08-07 21:46:47 -0500

    Peticiones

---
+++ peticiones/main.js
@@ -0,0 +1,37 @@
+/* let modulo = require("./modulo.js"); */
+
+//console.log(modulo);
+
+/* let resultado = modulo.restar(5,3);
+console.log(resultado); */
+
+const request = require('request');
+
+/* request.get('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+}); */
+
+
+/* request.get("https://swapi.co/api/people/10/", function(error, response, body) {
+  console.log("error:", error); // Print the error if one occurred
+  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  console.log("body:", body); // Print the HTML for the Google homepage.
+}); */
+
+// JSON = Java Script Object Notation
+// API = Application Programming Interface
+
+request.get("https://swapi.co/api/people/4/", function (error, response, body) {
+    if (response.statusCode == 200){
+        let respuesta = JSON.parse(body);
+        console.log(respuesta.films[0]);
+        
+        request.get(respuesta.films[0], function( error, response, body){
+            let respuesta = JSON.parse(body);
+            console.log(respuesta); 
+        })
+
+    } else console.log(response.statusCode, error);
+});
