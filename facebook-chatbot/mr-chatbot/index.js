80122ef3b4a77942d43fd972e5f3ec9e93af2af3
Masavi <masavi2008@hotmail.com>
2018-08-29 10:32:31 -0500

    Facebook Chatbot w/bootbot

---
+++ facebook-chatbot/mr-chatbot/index.js
@@ -0,0 +1,21 @@
+'use strict';
+const BootBot = require('bootbot');
+const config = require('config');
+
+const bot = new BootBot({
+  accessToken: config.get('accessToken'),
+  verifyToken: config.get('verifyToken'),
+  appSecret: config.get('appSecret')
+});
+
+/**
+ * Demo handler to echo back whatever the user says.
+ * Feel free to delete this handler and start hacking!
+ */
+
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Holiwis`);
+});
+
+bot.start(config.get('botPort'));
